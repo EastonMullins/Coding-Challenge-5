33effeac47faed883c534bf5f6c5dbc40f75107f
Easton <eastonmullins@usf.edu>
2025-02-09 11:09:05 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -9,3 +9,16 @@ console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
 
+//Task 2 - Object Methods
+
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder() {
+        console.log("ID: ", this.orderId);
+        console.log("Amount: ", this.totalAmount);
+        console.log("Status: ", this.status);
+    }
+}
+order.displayOrder();
