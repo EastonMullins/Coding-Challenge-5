a8f7f1f097b02b604bfac620343960bd908ad275
Easton <eastonmullins@usf.edu>
2025-02-09 11:01:49 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//Task 1 - Object Properties
+
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+}
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
